21bd8240cf75131f196c0a7339c6ddd920f34b13
usern132 <24841687+usern132@users.noreply.github.com>
2026-02-08 14:12:04 +0100

    Interfície d'usuari inicial sense funcionalitat

--- app/src/main/java/dk/itu/moapd/x9/s25137/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/x9/s25137/MainActivity.kt
@@ -1,20 +1,43 @@
 package dk.itu.moapd.x9.s25137
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.datepicker.MaterialDatePicker
+import dk.itu.moapd.x9.s25137.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.reportDateInput.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.show(supportFragmentManager, "datePicker")
+            datePicker.addOnPositiveButtonClickListener { selection ->
+                val date = Date(selection)
+                val format = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                binding.reportDateInput.setText(format.format(date))
+            }
+        }
+
+        val items = resources.getStringArray(R.array.report_types)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        binding.reportTypeInput.setAdapter(adapter)
     }
 }
