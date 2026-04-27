e8f514aeb569d83f1b613cd95ae0b71cff3ddbd3
usern132 <24841687+usern132@users.noreply.github.com>
2026-04-27 15:32:14 +0200

    Implementació inicial de GoogleMap

--- app/src/main/java/dk/itu/moapd/x9/s25137/ui/reports/map/ReportMap.kt
+++ app/src/main/java/dk/itu/moapd/x9/s25137/ui/reports/map/ReportMap.kt
@@ -1,9 +1,49 @@
 package dk.itu.moapd.x9.s25137.ui.reports.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import dk.itu.moapd.x9.s25137.ui.utils.PlaceholderScreen
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun ReportMap() {
-    PlaceholderScreen("Map")
+    val context = LocalContext.current
+    var hasPermission by remember {
+        mutableStateOf(
+            ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(52.52, 13.40), 10f)
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        uiSettings = MapUiSettings(
+            zoomControlsEnabled = false,
+            mapToolbarEnabled = false,
+        ),
+        properties = MapProperties(
+            isTrafficEnabled = true,
+            isMyLocationEnabled = hasPermission
+        )
+    )
 }
