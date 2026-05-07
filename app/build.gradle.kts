1e55efb03004eab599652a293f4431eedabe9123
usern132 <24841687+usern132@users.noreply.github.com>
2026-05-07 16:32:48 +0200

    Desactivar ProGuard per evitar problemes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,11 +28,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
